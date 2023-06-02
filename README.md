830161a12b4e03611d2e8ad898a787293fbce68e
Sean Galland <s_galland@msn.com>
2023-06-02 15:59:54 -0600

    Added string_reverse.zig and updated the README.md

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Repository of Zig code examples that I have cooked up learning Zig.
 
 To run the examples use the following command: `zig run .\src\<filename>.zig`
 
+To run the tests use the following command: `zig test .\src\<filename>.zig`
+
 ## Examples
 
 1. fizzbuzz.zig: An example implementation of [Fizz Buzz](https://en.wikipedia.org/wiki/Fizz_buzz).
+1. string_reverse.zig: Reverse a string by iterating half way through and swapping values. Should be able to reverse any string of any length. [Reverse String](https://www.geeksforgeeks.org/reverse-a-string-in-c-cpp-different-methods/).
---
+++ src/string_reverse.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+fn findArrayMid(comptime T: type, original: []const T) usize {
+    var mid: f32 = @intToFloat(f32, original.len) / 2;
+    return @floatToInt(usize, @round(mid));
+}
+
+/// Optimally reverses a string.
+fn optimizedStringReverse(allocator: std.mem.Allocator, string: []const u8) ![]u8 {
+    var i: usize = 0;
+    var data: []u8 = try allocator.alloc(u8, string.len);
+
+    while (i < findArrayMid(u8, string)) : (i += 1) {
+        var temp = string[i];
+        data[i] = string[data.len - 1 - i];
+        data[data.len - 1 - i] = temp;
+    }
+
+    return data;
+}
+
+pub fn main() !void {
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = general_purpose_allocator.allocator();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const result = try optimizedStringReverse(allocator, "sdrawkcab os leef I");
+    std.debug.print("{s}\n", .{result});
+}
+
+test "test optimizedStringReverse" {
+    const expect = std.testing.expect;
+    const eql = std.mem.eql;
+
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = general_purpose_allocator.allocator();
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const result = try optimizedStringReverse(allocator, "sdrawkcab os leef I");
+    try expect(eql(u8, result, "I feel so backwards"));
+
+    const result2 = try optimizedStringReverse(allocator, ".smaerd ruoy naht retteb yllanif si ytilaer esuaceb peelsa llaf t'nac uoy nehw evol ni er'uoy wonk uoY");
+    try expect(eql(u8, result2, "You know you're in love when you can't fall asleep because reality is finally better than your dreams."));
+}
